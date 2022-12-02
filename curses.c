86300697e47985c69523e686b3c5a5094bab956b
Henrique André <henrique_andre10@hotmail.com>
2022-12-02 11:27:03 -0300

    fixed bugs

--- curses.c
+++ curses.c
@@ -13,3 +13,15 @@ int main( int argc, char ** argv ){
     int row,col;
     // starts ncurses lib
     initscr();
+    // get widow size and put it in row and col
+    getmaxyx(stdscr,row,col);
+    mvprintw(row/2,(col-strlen(mesg))/2,"%s",mesg);
+    getstr(str);
+    attron(A_BOLD);
+    //print mensage
+    mvprintw(0, (col-strlen("Você digitou: "))/2, "Você digitou: %s", str);
+    getch();
+    endwin();
+
+    return 0;
+}

cc94f5ada5fd95e64e2a58e8f0fee3b1913eac52
Henrique André <henrique_andre10@hotmail.com>
2022-12-02 10:54:38 -0300

    implement window manipulation

--- curses.c
+++ curses.c
@@ -1,15 +1,32 @@
 #include <ncurses.h>
+#include <string.h>
 
-//testing curses library for linux 
+//This function shows some uses to ncurses lib in linux. 
+//the most of uses is window manipulation.
 
-int main (){
+int main( int argc, char ** argv ){
+
+    // Mensagem aparece na tela
+    char mesg[]="Digite uma mensagem: ";
+    char str[80];
+
+    // para armazenar o número de linhas e o número de colunas da tela
+    int row,col;
+
+    // inicia a ncurses
     initscr();
-    printw("testing");
-    getch();
-    refresh();
-    getch();
-    printw("a");
+
+    // obtém o número de linhas e colunas
+    getmaxyx(stdscr,row,col);
+
+    // imprime a mensagem no centro da tela
+    mvprintw(row/2,(col-strlen(mesg))/2,"%s",mesg);
+    getstr(str);
+    attron(A_BOLD);
+    mvprintw(0, (col-strlen("Você digitou: "))/2, "Você digitou: %s", str);
     getch();
     endwin();
+
     return 0;
 }
+
